fe511067b76ad82d44b66d8e3392a417bf8d94f8
MRH0 <mrhmail1@gmail.com>
2021-02-13 17:30:56 +0100

    v1

---
+++ src/translation.js
@@ -0,0 +1,30 @@
+let current = null;
+
+export async function initLang() {
+    let params = new URLSearchParams(window.location.search);
+    await setLang(params.get("lang") || "en_us");
+}
+
+export async function setLang(name) {
+    current = null;
+    try {
+        current = await (await fetch("lang/" + name + ".json")).json();
+    }
+    catch(e) {
+        console.error(e);
+        current = {};
+    }
+}
+
+export function T(key, values = {}) {
+    let r = current[key];
+    if(r == null)
+        return key;
+    for(let v in values) {
+        console.log(v)
+        r = r.replaceAll("{"+v+"}", values[v]);
+    }
+    return r;
+}
+
+export default T;
