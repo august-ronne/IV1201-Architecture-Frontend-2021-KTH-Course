04a82a16968249e5c53cc9b567d1eb5af82af2f6
MRH0 <mrhmail1@gmail.com>
2021-02-28 12:14:13 +0100

    translations

--- src/Models/RegisterFormModel.js
+++ src/Models/RegisterFormModel.js
@@ -7,15 +7,15 @@ const RegisterSchema = yup.object().shape({
     firstName: yup
         .string("Text only")
         .matches(/^[a-z]*$/, "error.lowercase")// "Only enter lowercase letters"
-        .min(2, "Your first name must be at least 2 characters")
-        .max(50, "Your last name must be 50 characters or shorter")
-        .required("You must enter your first name"),
+        .min(2, "error.short") // "Your first name must be at least 2 characters"
+        .max(50, "error.long") // "Your last name must be 50 characters or shorter"
+        .required("error.validFirstname"), // "You must enter your first name"
     lastName: yup
         .string()
-        .matches(/^[a-z]*$/, "Only enter lowercase letters")
-        .min(2, "Your last name must be at least 2 characters")
-        .max(50, "Your last name must be 50 characters or shorter")
-        .required("You must enter your last name"),
+        .matches(/^[a-z]*$/, "error.lowercase") // "Only enter lowercase letters"
+        .min(2, "error.short") // "Your last name must be at least 2 characters"
+        .max(50, "error.long") // "Your last name must be 50 characters or shorter"
+        .required("error.validLastname"), // "You must enter your last name"
     email: yup
         .string()
         .email("error.validEmail")
@@ -29,18 +29,18 @@ const RegisterSchema = yup.object().shape({
         .required("error.validUsername")
         .matches(
             /^[a-zA-Z0-9]+$/,
-            "Only lowercase and capital letters, as well as numbers are allowed"
+            "error.validPassword" // "Only lowercase and capital letters, as well as numbers are allowed"
         ),
     password: yup
         .string()
         .min(6)
         .max(1024)
-        .required("You must enter a password"),
+        .required("error.validPassword"), // "You must enter a password"
     confirmPassword: yup
         .string()
         .oneOf(
             [yup.ref("password"), null],
-            "Password confirmation is not the same as the first password you entered"
+            "error.passwordMissmatch" // "Password confirmation is not the same as the first password you entered"
         ),
 });
 
